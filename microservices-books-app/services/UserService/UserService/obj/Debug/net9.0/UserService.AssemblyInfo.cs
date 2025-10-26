889e661d293c4da001cf169cf86d73c120c91c09
bahagh <73429122+bahagh@users.noreply.github.com>
2025-10-26 20:58:08 +0100

    bks users fixtry

--- microservices-books-app/services/UserService/UserService/obj/Debug/net9.0/UserService.AssemblyInfo.cs
+++ microservices-books-app/services/UserService/UserService/obj/Debug/net9.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e27e2f0a34ef183e8a992aae12f4c74cbfaf8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d8a194b268f80ea56d5c7eb4d0c92b91627ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
